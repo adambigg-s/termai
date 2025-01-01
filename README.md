6c363f157d23f773386f45d8980c4c4add7c5bd2
abiggs33 <146982420+abiggs33@users.noreply.github.com>
2024-12-31 20:19:38 -0500

    opens text editor now

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # termai
 just allows terminal interfacing with xai's Grok
+
+currently, operates under the assumption that the user will be using the Helix
+text editor. this is what i use. this could be easily modified to be open
+any other text editor
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 
 
 import sys
+import os
+import subprocess
 import requests
 
 
 API_KEY: str = open("xai.key", "r").read().strip()
 GROK_URL: str = "https://api.x.ai/v1/chat/completions"
 INIT_PROMPT: str = open("personal_prompt.key", "r").read().strip()
+QUERY_FILE = "query.key"
+EDITOR_ALIAS = "hx"
 
 
 class Grok:
@@ -40,12 +44,34 @@ class Grok:
         return ans.json()['choices'][0]['message']['content']
 
 
+def open_with_editor(cleared: bool):
+    if cleared:
+        with open(QUERY_FILE, "w") as file:
+            file.write("")
+    subprocess.run([EDITOR_ALIAS, QUERY_FILE], check=True)
+
+
 def main():
     grok = Grok()
-    query = grok.get_query()
-    reponse = grok.grok_query(query)
 
-    print(reponse)
+    if not os.path.exists(QUERY_FILE):
+        with open(QUERY_FILE, "w") as file:
+            file.write("")
+
+    print("opening helix to prompt Grok...")
+    open_with_editor(QUERY_FILE)
+
+    with open(QUERY_FILE, "r") as file:
+        query = file.read().strip()
+
+    if not query:
+        print("no query provied, therefore exit")
+        return
+
+    full_query = INIT_PROMPT + " " + query
+
+    response = grok.grok_query(full_query)
+    print("\n", response)
 
 
 if __name__ == '__main__':
